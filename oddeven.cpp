7a1e40f35f43af3302f15c785f66806b5fddc057
15EC049 <umarbasheer1996@gmail.com>
2016-09-22 14:23:10 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+cout<<"\nenter a number:";
+cin>>a;
+if(a%2==0)
+ cout<<"\nthe number is even";
+else 
+ cout<<"\n the number is odd";
+getch();
+}
